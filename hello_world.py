a48679a0f8a1f585df891f77b7304bd5a668b2db
Ankan-427 <83961097+Ankan-427@users.noreply.github.com>
2021-05-26 20:19:48 +0530

    hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Display HELLO WORLD on screen
+#print is a Python defined function
+print('HELLO WORLD')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
